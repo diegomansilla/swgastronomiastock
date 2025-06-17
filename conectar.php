1d93d2c8711b106375e4db3d98cd8151cc7f40af
Alesio <alesio577@gmail.com>
2025-06-17 14:37:07 -0300

    se agrega el archivo conectar.php

---
+++ conectar.php
@@ -0,0 +1,9 @@
+<?php
+$servidor = "localhost";    
+$usuario = "alesio";          
+$password = "hero2017";             
+$base = "bbdd_gestion";          
+
+$connection = new mysqli($servidor, $usuario, $password, $base);
+?>
+
