142e2f2163843ea67311b78f8dffa1db056c4ccc
Alesio <alesio577@gmail.com>
2025-08-25 10:38:07 -0300

    modified:   gestion_alertas/alertas.php modified:   gestion_alertas/consulta_alertas.php

--- gestion_alertas/alertas.php
+++ gestion_alertas/alertas.php
@@ -10,23 +10,11 @@ include("consulta_alertas.php");
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Alertas</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    
-    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.7/dist/morph/bootstrap.min.css">
-    
-    <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    
-    <!-- DataTables CSS -->
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
-    
-    <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-    
-    <!-- DataTables JS -->
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-    
-    <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
@@ -91,7 +79,7 @@ include("consulta_alertas.php");
     </div>
     <!-- Fin Modal -->
 
-    <!-- Tabla principal (modificable) -->
+    <!-- Tabla principal -->
     <table id="tablaAlertas" class="table table-striped">
         <thead>
             <tr>
